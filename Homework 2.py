6a955127077239ae8e34ff475c4eb1d64ab51aad
alex <alexey_str@yahoo.com>
2023-01-30 00:20:15 +0100

    1

---
+++ Homework 2.py
@@ -0,0 +1,46 @@
+# import this
+# import antigravity
+# import __hello__
+# from __future__ import braces
+from math import sqrt
+
+
+def task2a():
+    a = 1
+    b = 5
+    c = 4
+
+    x = (-b + sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    print(x)
+    x = (-b - sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    print(x)
+
+
+task2a()
+
+
+def task2b():
+    a = 2
+    b = 5
+    c = 2
+
+    x = (-b + ((b ** 2 - 4 * a * c) ** 0.5)) / (2 * a)
+    print(x)
+    x = (-b - ((b ** 2 - 4 * a * c) ** 0.5)) / (2 * a)
+    print(x)
+
+
+task2b()
+
+
+def task3():
+
+
+
+
+
+
+
+
+
+task3()
