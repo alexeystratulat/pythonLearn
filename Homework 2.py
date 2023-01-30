e6ae3ec6902c0d15a5187cee02cdef13c54fa15d
alex <alexey_str@yahoo.com>
2023-01-30 20:46:36 +0100

    1

--- Homework 2.py
+++ Homework 2.py
@@ -1,8 +1,25 @@
+import requests
+import datetime
 # import this
 # import antigravity
 # import __hello__
 # from __future__ import braces
+from bs4 import BeautifulSoup
+from art import *
+
 from math import sqrt
+
+
+def currensy():
+    url = 'https://minfin.com.ua/currency/nbu'
+    source = requests.get(url)
+    soup = BeautifulSoup(source.content, 'html.parser')
+    table = soup.find("table", {"class": "sc-1x32wa2-1 dYkgjk"})
+    tr = table.find("p", {"class": "sc-1x32wa2-9 glerpA"})
+    tr = tr.text[:5]
+    tr = tr.replace(",", ".")
+    tr = float(tr)
+    return (tr)
 
 
 def task2a():
@@ -34,13 +51,27 @@ task2b()
 
 
 def task3():
+    a = str(input("Введіть ваше ім'я: "))
 
-
-
-
-
-
-
+    print('Привіт,', a)
 
 
 task3()
+
+
+def task4():
+    now = datetime.datetime.now()
+    a = (input("Cкiльки у Вас гривень ? "))
+    result = int(a) / (currensy())
+    print('Станом на ', now.date(), 'це становить ', result, 'доларiв США')
+
+
+task4()
+
+
+
+def task5():
+    tprint("alex", font= "rnd-xlarge")
+
+
+task5()
