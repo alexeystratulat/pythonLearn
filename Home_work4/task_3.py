943e09090d0f32667f5e46f6620b109d6d00c731
alex strat <alexeystrat@gmail.com>
2023-02-07 14:24:24 +0100

    4 3

--- Home_work4/task_3.py
+++ Home_work4/task_3.py
@@ -1,1 +1,14 @@
-#
+"""
+Напишіть програму, яка зчитує ціле число та виводить текст, аналогічний наведеному у прикладі (пробіли та переведення на новий рядок важливі!).
+Перший рядок містить наступне значення, а другий рядок містить попереднє значення введеного числа:
+    Please enter an integer number: 1234
+    Next number for number 1234 is 1235.
+    Previous number for number 1234 is 1233.
+"""
+while True:
+    try:
+        a = int(input('Please enter an integer number: '))
+        print('Next number for number ',a,'is ',a+1,'\n' 'Previous number for number ',a,'is ',a-1)
+        break
+    except ValueError:
+        print('not an integer')
