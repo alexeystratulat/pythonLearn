33a09872c7e96243fdb683d39a47247831a77f06
alex <alexey_str@yahoo.com>
2023-02-11 23:51:14 +0100

    5 completed

--- Home_work5/task2.py
+++ Home_work5/task2.py
@@ -6,7 +6,7 @@
 import math
 
 
-def triangle(a, b):
+def triangle_square_and_perimeter(a, b):
     perimetr = a + b + (math.sqrt(a ** 2 + b ** 2))
     area = (a * b) / 2
 
@@ -15,4 +15,5 @@ def triangle(a, b):
 
 a = float(input("введіть довжиру першого катета "))
 b = float(input("тепер- другого "))
-print("Площа трикутника = ", triangle(a, b)[0], "Периметр = ", triangle(a, b)[1])
+print("Площа трикутника = ", triangle_square_and_perimeter(a, b)[0], "Периметр = ",
+      triangle_square_and_perimeter(a, b)[1])
