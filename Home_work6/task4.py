54fc35400e203539aaf80de00795e1088480207a
alex strat <alexeystrat@gmail.com>
2023-02-21 10:43:58 +0100

    in prog

--- Home_work6/task4.py
+++ Home_work6/task4.py
@@ -10,15 +10,7 @@ import math
 
 def circles_intersect(x1, y1, r1, x2, y2, r2):
     d = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
-    if d > r1 + r2:
-        return True
-    elif d < r2 - r1:
-        return True
-    elif d > r1 + r2:
-        return True
-    else:
-        return False
-
+    return d > r1 + r2 or d < r2 - r1 or d < r1 - r2
 
 def main():
     x1, y1, r1, x2, y2, r2 = map(int, input("Введи 6 значений: ").split())
