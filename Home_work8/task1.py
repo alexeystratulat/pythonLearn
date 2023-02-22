0c143b806ea5b93814cb49fa0a359441d0b3c6c9
alex <alexey_str@yahoo.com>
2023-02-22 09:53:44 +0100

    not enough

---
+++ Home_work8/task1.py
@@ -0,0 +1,8 @@
+"""
+Дано довільний список. Створити функцію index(lst, elem), що повертатиме індекс elem в списку lst, або, якщо елемент відсутній, то None.
+Не можна використовувати метод list().index().
+
+def index(lst, elem):  # returns integer or None
+    pass
+"""
+
