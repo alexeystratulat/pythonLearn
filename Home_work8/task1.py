d09ef58d0b13697b0ddbd83a03fdddd23671d169
alex strat <alexeystrat@gmail.com>
2023-02-23 17:03:58 +0100

    in prog

--- Home_work8/task1.py
+++ Home_work8/task1.py
@@ -6,3 +6,19 @@ def index(lst, elem):  # returns integer or None
     pass
 """
 
+
+def index(lst, elem):  # returns integer or None
+    for i in range(0, len(lst)):
+        if i == elem:
+            return i
+
+    return None
+
+
+def main():
+    numbers = [0, 1, 2, 3, 4, 5, 6, 5, 8, 9]
+    print(index(numbers, 5))
+
+
+if __name__ == '__main__':
+    main()
--- Home_work8/task4.py
+++ Home_work8/task4.py
@@ -4,3 +4,25 @@
 def gen_primes():  # returns list of ints
     pass
 """
+
+
+def gen_primes():
+    list = []
+    for i in range(2, 100):
+        counter = 0
+        for j in range(i - 1, 1, -1):
+
+            if (i % j) == 0:
+                counter += 1
+        if counter == 0:
+            list.append(i)
+
+    return list
+
+
+def main():
+    print(gen_primes())
+
+
+if __name__ == '__main__':
+    main()
