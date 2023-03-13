d6f9926cd7beb98d10db82800f4078a0d5d928d8
alex <alexey_str@yahoo.com>
2023-03-13 18:06:37 +0100

    10.3 done

--- Home_work10/task3.py
+++ Home_work10/task3.py
@@ -7,7 +7,8 @@
 
 
 Напишіть функцію, яка прийматиме рядок тексту та перетворюватиме його способом, cхожим до описаного вище.
-В якості алгоритму перемішування букв обов'язково використовуйте наступні кроки для кожного слова в тексті (треба дотриматися цих вимог та алгоритму, а також запропонованої сигнатури функції):
+В якості алгоритму перемішування букв обов'язково використовуйте наступні кроки для кожного слова в тексті (треба дотриматися цих вимог та алгоритму,
+а також запропонованої сигнатури функції):
 
 Для простоти вважати, що пунктуація є "буквами" і частинами слів, до яких вона торкається.
 Але, за бажанням, можна обробити лише слова, а пунктуацію лишити на правильних місцях.
@@ -18,3 +19,47 @@
 def pemrtuate(text):  # returns permuted text
     pass
 """
+
+
+def pemrtuate(text):  # returns permuted text
+    text = text.split()
+    text_to_change = list(text)
+    text_to_print = []
+    for i in text_to_change:
+        text_to_print.append(change(i))
+
+    print(" ".join(text_to_print))
+
+
+def change(word):
+
+    if len(word) > 3:
+        word_for_return = [None for x in range(len(word))]
+        word_for_return[0] = word[0]
+        word_for_return[len(word) - 1] = word[len(word) - 1]
+
+        for i in range((len(word)) // 3):
+
+            word_for_return[i + 1] = word[len(word) - 2 - i]
+            word_for_return[len(word) - 2 - i] = word[i + 1]
+
+
+        if word_for_return[(len(word))//2] is None:
+            word_for_return[(len(word))//2] = word[(len(word))//2]
+
+
+
+
+
+        return("".join(word_for_return))
+    else:
+        return word
+
+
+def main() -> None:
+    text = "The list methods make it very easy to use a list as a stack where the last element added is the first element retrieved"
+    pemrtuate(text)
+
+
+if __name__ == "__main__":
+    main()
